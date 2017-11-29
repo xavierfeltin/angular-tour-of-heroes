5f8eb7fa5c89684d7383b756a6ab8462d3389110
xavierfeltin <xavier.feltin@gmail.com>
2017-11-29 08:35:04 +0100

    implemented routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,18 +9,13 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-
+  
   constructor(private heroService: HeroService) {}
   
   ngOnInit() {
     this.getHeroes();
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
+  
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
